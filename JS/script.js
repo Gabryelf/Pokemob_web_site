cbf2c43ed41e00fc575da4a63b7cbca0fbc66c6a
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2025-04-06 10:11:56 +0300

    Initial_commit
    _create_repo
    _add_start_project_file

---
+++ JS/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $('.carousele').slick({
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        slidesToShow: 3, // Показываем 3 элемента
+        slidesToScroll: 1, // Прокручиваем по одному
+        centerMode: true, // Центральный элемент больше
+        variableWidth: false, // Отключаем, чтобы все элементы были одинаковой ширины
+        focusOnSelect: true, // Выбор центрального элемента при клике
+        autoplaySpeed: 2000 // Скорость смены слайдов
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const themeToggle = document.getElementById("theme-toggle");
+    const themeIcon = document.getElementById("theme-icon");
+    const body = document.body;
+
+    // Проверяем, какая тема была выбрана ранее
+    if (localStorage.getItem("darkMode") === "enabled") {
+        body.classList.add("dark-mode");
+        themeIcon.src = "IMG/sun.png"; // Меняем иконку
+    }
+
+    themeToggle.addEventListener("click", function() {
+        body.classList.toggle("dark-mode");
+
+        // Меняем иконку при смене темы
+        if (body.classList.contains("dark-mode")) {
+            themeIcon.src = "IMG/sun.png";
+            localStorage.setItem("darkMode", "enabled");
+        } else {
+            themeIcon.src = "IMG/moon.png";
+            localStorage.setItem("darkMode", "disabled");
+        }
+    });
+});
